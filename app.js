406169e26342bc93ba1b97ca41d9a216ae1bfe99
espartacoparra <espartaco.ing@gmail.com>
2020-02-12 00:48:56 -0400

    a

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser')
 const Middlewares = require('./src/middlewares/middlewares');
 const cors = require('cors');
 const app = express();
-app.use(express.static('public'));
+//app.use(express.static('public'));
 app.use(cors());
 app.use(bodyParser.json({ limit: '10mb' }));
 app.set("port", process.env.Port || 3000);
@@ -15,7 +15,7 @@ app.get('/hola',(req,res)=>{
 });
 app.use('/api', sessionRoutes);
 app.use('/api', Middlewares.apitoken, router);
-app.get('/*',(req,res)=>res.sendFile(path.join(__dirname+'/public/index.html')));
+//app.get('/*',(req,res)=>res.sendFile(path.join(__dirname+'/public/index.html')));
 
 app.listen(app.get('port'), () => {
     console.log('Corriendo en el puerto :' + app.get('port'));
