fb095f06951b12f03ef6914b1328857473d3dbf4
espartacoparra <espartaco.ing@gmail.com>
2020-02-12 00:33:32 -0400

    a

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get('/hola',(req,res)=>{
 });
 app.use('/api', sessionRoutes);
 app.use('/api', Middlewares.apitoken, router);
-//app.get('/*',(req,res)=>res.sendFile(path.join(__dirname+'/public/index.html')));
+app.get('/*',(req,res)=>res.sendFile(path.join(__dirname+'/public/index.html')));
 
 app.listen(app.get('port'), () => {
     console.log('Corriendo en el puerto :' + app.get('port'));
